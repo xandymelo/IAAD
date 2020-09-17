d111d73e39dd489e026dc76dae74f2b514e24cb0
Alexandre Melo <xandymelo@outlook.com.br>
2020-09-17 16:16:23 -0300

    Add files via upload

---
+++ Semana05/2b1 - semana 05.sql
@@ -0,0 +1,1 @@
+SELECT nome_programador, nome_startup FROM PROGRAMADOR, STARTUP WHERE STARTUP.id_startup = PROGRAMADOR.id_startup
---
+++ Semana05/2b2 - semana 05.sql
@@ -0,0 +1,1 @@
+SELECT nome_programador, nome_startup FROM PROGRAMADOR INNER JOIN startup ON STARTUP.id_startup = PROGRAMADOR.id_startup
