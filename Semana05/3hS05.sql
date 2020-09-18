f96d44265e8092e7d9e20cbd95a9961b439d33e9
Alexandre Melo <xandymelo@outlook.com.br>
2020-09-18 19:07:20 -0300

    Update 3hS05.sql

--- Semana05/3hS05.sql
+++ Semana05/3hS05.sql
@@ -1,3 +1,3 @@
-SELECT Projnome as 'Nome do projeto', Dnome as 'Nome do departamento', H.countresult as 'Número de Funcionarios' , H.hor as 'Horas trabalhadas'
+CREATE VIEW PROJETO_FUNCIONARIOS_HORAS AS SELECT Projnome as 'Nome do projeto', Dnome as 'Nome do departamento', H.countresult as 'Número de Funcionarios' , H.hor as 'Horas trabalhadas'
 FROM PROJETO P ,DEPARTAMENTO  D, (SELECT count(Fcpf) as 'countresult', Pnr, sum(Horas) as 'hor' FROM TRABALHA_EM TE GROUP BY Pnr) H
 WHERE P.Dnum = D.Dnumero AND P.Projnumero = H.Pnr
